999eaf210e5fbdabb33952d68d1696b735706133
Bahlasz <you@example.com>
2022-05-14 13:42:32 +0200

    first commit

---
+++ test_game2/main.py
@@ -0,0 +1,31 @@
+import pygame
+import sys
+from settings import WIDTH, HEIGTH, FPS
+from level import Level
+
+
+class Game:
+    def __init__(self) -> None:
+
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGTH))
+        pygame.display.set_caption("test game2")
+        self.clock = pygame.time.Clock()
+        self.level = Level()
+
+    def run(self):
+        game_is_active = True
+        while game_is_active:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            self.screen.fill("black")
+            self.level.run()
+            pygame.display.update()
+            self.clock.tick(FPS)
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
---
+++ test_game2/settings.py
@@ -0,0 +1,13 @@
+# game setup
+WIDTH = 1600
+HEIGTH = 800
+FPS = 60
+TILESIZE = 64
+
+# weapons
+weapon_data = {
+    'sword': {'cooldown': 100, 'damage': 15, 'graphic': '../graph/weapons/sword/full.png'},
+    'lance': {'cooldown': 400, 'damage': 30, 'graphic': '../graph/weapons/lance/full.png'},
+    'axe': {'cooldown': 300, 'damage': 20, 'graphic': '../graph/weapons/axe/full.png'},
+    'rapier': {'cooldown': 50, 'damage': 8, 'graphic': '../graph/weapons/rapier/full.png'},
+    'sai': {'cooldown': 80, 'damage': 10, 'graphic': '../graph/weapons/sai/full.png'}}
