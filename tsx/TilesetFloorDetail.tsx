f48757f6d04b8c1589a7ed34a3f05b39fff22791
Bahlasz <you@example.com>
2022-05-16 11:39:14 +0200

    file structure fixed

---
+++ tsx/TilesetFloorDetail.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="TilesetFloorDetail" tilewidth="128" tileheight="128" tilecount="80" columns="16">
+ <image source="../valid_tileset_imagees/TilesetFloorDetail.png" width="2048" height="640"/>
+ <tile id="37" probability="0.5"/>
+</tileset>
---
+++ tsx/objects.tsx
@@ -0,0 +1,67 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="objects" tilewidth="128" tileheight="128" tilecount="21" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="128" height="128" source="../valid_tileset_imagees/objects/0.png"/>
+ </tile>
+ <tile id="1">
+  <image width="128" height="128" source="../valid_tileset_imagees/objects/01.png"/>
+ </tile>
+ <tile id="2">
+  <image width="128" height="128" source="../valid_tileset_imagees/objects/02.png"/>
+ </tile>
+ <tile id="3">
+  <image width="128" height="128" source="../valid_tileset_imagees/objects/03.png"/>
+ </tile>
+ <tile id="4">
+  <image width="128" height="128" source="../valid_tileset_imagees/objects/04.png"/>
+ </tile>
+ <tile id="5">
+  <image width="128" height="128" source="../valid_tileset_imagees/objects/05.png"/>
+ </tile>
+ <tile id="6">
+  <image width="128" height="128" source="../valid_tileset_imagees/objects/06.png"/>
+ </tile>
+ <tile id="7">
+  <image width="128" height="128" source="../valid_tileset_imagees/objects/07.png"/>
+ </tile>
+ <tile id="8">
+  <image width="128" height="128" source="../valid_tileset_imagees/objects/08.png"/>
+ </tile>
+ <tile id="9">
+  <image width="128" height="128" source="../valid_tileset_imagees/objects/09.png"/>
+ </tile>
+ <tile id="10">
+  <image width="128" height="128" source="../valid_tileset_imagees/objects/10.png"/>
+ </tile>
+ <tile id="11">
+  <image width="64" height="128" source="../valid_tileset_imagees/objects/11.png"/>
+ </tile>
+ <tile id="12">
+  <image width="64" height="128" source="../valid_tileset_imagees/objects/12.png"/>
+ </tile>
+ <tile id="13">
+  <image width="64" height="128" source="../valid_tileset_imagees/objects/13.png"/>
+ </tile>
+ <tile id="14">
+  <image width="64" height="128" source="../valid_tileset_imagees/objects/14.png"/>
+ </tile>
+ <tile id="15">
+  <image width="128" height="128" source="../valid_tileset_imagees/objects/15.png"/>
+ </tile>
+ <tile id="16">
+  <image width="128" height="128" source="../valid_tileset_imagees/objects/16.png"/>
+ </tile>
+ <tile id="17">
+  <image width="128" height="128" source="../valid_tileset_imagees/objects/17.png"/>
+ </tile>
+ <tile id="18">
+  <image width="128" height="128" source="../valid_tileset_imagees/objects/18.png"/>
+ </tile>
+ <tile id="19">
+  <image width="128" height="128" source="../valid_tileset_imagees/objects/19.png"/>
+ </tile>
+ <tile id="20">
+  <image width="128" height="128" source="../valid_tileset_imagees/objects/20.png"/>
+ </tile>
+</tileset>
