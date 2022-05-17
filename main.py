273e44bf34fb0ed4f216638ac4a1fbc6091d2ce5
appuser <appuser@journey2-7857cc5545-496xt>
2022-05-17 11:16:17 +0000

    starter project

---
+++ main.py
@@ -0,0 +1,43 @@
+import util
+import engine
+import ui
+
+PLAYER_ICON = '@'
+PLAYER_START_X = 3
+PLAYER_START_Y = 3
+
+BOARD_WIDTH = 30
+BOARD_HEIGHT = 20
+
+
+def create_player():
+    '''
+    Creates a 'player' dictionary for storing all player related informations - i.e. player icon, player position.
+    Fell free to extend this dictionary!
+
+    Returns:
+    dictionary
+    '''
+    pass
+
+
+def main():
+    player = create_player()
+    board = engine.create_board(BOARD_WIDTH, BOARD_HEIGHT)
+
+    util.clear_screen()
+    is_running = True
+    while is_running:
+        engine.put_player_on_board(board, player)
+        ui.display_board(board)
+
+        key = util.key_pressed()
+        if key == 'q':
+            is_running = False
+        else:
+            pass
+        util.clear_screen()
+
+
+if __name__ == '__main__':
+    main()
