dd5bf3038e0b7c39f4e0ddfac1b6bb2e1abc455a
MattheWhite <fehermatyas97@gmail.com>
2023-04-03 22:54:50 +0200

    Add the map configs for rendering

---
+++ tsx/TilesetFloorDetail64.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="TilesetFloorDetail64" tilewidth="64" tileheight="64" tilecount="80" columns="16">
+ <image source="../valid_tileset_imagees/TilesetFloorDetail64.png" width="1024" height="320"/>
+ <tile id="37" probability="0.7"/>
+ <tile id="38" probability="0.7"/>
+ <tile id="39" probability="0.7"/>
+</tileset>
---
+++ tsx/floor2.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="TilesetFloor64" tilewidth="64" tileheight="64" tilecount="572" columns="22">
+ <image source="../valid_tileset_imagees/TilesetFloor64.png" width="1408" height="1668"/>
+ <tile id="407" probability="0.5"/>
+</tileset>
